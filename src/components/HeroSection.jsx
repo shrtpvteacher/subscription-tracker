13f4b6fe14465bb0f8b56535d483a310190c31d1
Kelly Anne <shrtpvteacher@protonmail.com>
2025-07-15 14:20:29 -0700

    Fix HeroSection syntax error

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -24,7 +24,6 @@ export function HeroSection() {
     <>
       <div className="relative overflow-hidden bg-gradient-to-br from-gray-950 via-gray-900 to-gray-950">
         {/* Background Pattern */}
-        <div className="absolute inset-0 bg-gradient-to-r from-indigo-600/10 to-purple-600/10"></div>
        
 
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-20 lg:py-24">
